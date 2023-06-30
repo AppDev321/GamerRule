9d65b746ed8efbb52d23c5d6815972cb629ef5b0
tanay388 <tanay.deo388@gmail.com>
2023-06-30 13:04:18 +0530

    Add: Admin Screen( Add new Game)

---
+++ app/src/main/java/com/gamerrule/android/ui/admin/AddNewGameActivity.java
@@ -0,0 +1,16 @@
+package com.gamerrule.android.ui.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gamerrule.android.R;
+
+public class AddNewGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_game);
+    }
+}
