440d55e9c798c3da09b7d0ec87064ef6148cbd3a
tanay388 <tanay.deo388@gmail.com>
2023-07-02 10:49:13 +0530

    Add: Add Homescreen

---
+++ app/src/main/java/com/gamerrule/android/ui/users/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.gamerrule.android.ui.users;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gamerrule.android.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
